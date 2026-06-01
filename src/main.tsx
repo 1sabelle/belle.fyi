6e7bd4f39da6d99fc8ade0a608a2fad09bbe66d4
Isabelle Schafhauser <isabelle.schafhauser@gmail.com>
2026-06-01 23:28:10 +0200

    :tada: init belle.fyi

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import '@fontsource/gilda-display/index.css'        
+import '@fontsource/harmattan/index.css'     
+import '@fontsource/instrument-sans/400.css'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
